574499ecdb2d892cdd2dc5fd99a84c032d4bed88
Jubair <jubair.ahmed7946@gmail.com>
2024-01-10 22:42:51 +0600

    Add: tasks get api created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 const app = express()
@@ -8,6 +9,44 @@ const port = process.env.PORT || 5000;
 // middlewares
 app.use(cors())
 app.use(express.json())
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // DB: tasks collection
+    const tasksCollection = client.db("taskifyDB").collection("tasks")
+
+    // GET; all the tasks
+    app.get("/tasks", async(req, res)=> {
+        const result = await tasksCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get("/", (req,res)=>{
     res.send("Taskify is running")
